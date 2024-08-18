15d3e8f5e81411383da01cb683d6f4429e1380f2
Samuel Azevedo <samueltrader931@gmail.com>
2024-08-18 16:11:35 -0300

    Create src folder and install dependencies

---
+++ src/models/task.entity.ts
@@ -0,0 +1,15 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export default class Task extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    title!: string
+
+    @Column({default: false})
+    completed!: boolean
+
+}
